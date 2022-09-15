a98a52a335d2944f2df54d0817b2690d6b7c9585
kaioSantos32 <kaiohenriquesanto@gmail.com>
2022-09-15 17:22:41 -0300

    Melhorando trabalho com indices monetarios

--- src/main/kotlin/br/com/alura/array/Ranges.kt
+++ src/main/kotlin/br/com/alura/array/Ranges.kt
@@ -1,39 +1,39 @@
 package br.com.alura.array
 
-fun main(){
-    val serie: IntRange= 1.rangeTo(other = 10)
-    for(s in serie) {
-        println("$serie ")
+fun main() {
+    val serie: IntRange = 1..10
+    for (i in serie) {
+        print("$i ")
     }
-    // Toda essa função de declaração `rangeTo`, pode ser resumida por:
-    val serie1: IntRange = 1..10
-    println(serie1.max()/2)
 
+    for (i in 1..100) {
+        if(i % 2 == 0) {
+            print("$i ")
+        }
+    }
 
-    val numerosPares1: IntProgression = 2..100 step 2 // Imprime numeros de 2 a 100, incluindo o 100
-    val numerosPares2: IntProgression = 2.until(to = 100) step 2 // Imprime numeros de 2 a 100, não incluindo o 100
+    val numerosPares: IntProgression = 2..100 step 2
+    numerosPares.forEach { print("$it ") }
 
-    numerosPares1.forEach { print("$it ") } // melhor forma de imprimir valores em arrays
-//    for (numero: Int in numerosPares1){
-//        print("$numero ")
-//    }
-    println()
-    val numerosParesReverso: IntProgression = 100 downTo 0 step 2
+    val numerosParesReverso: IntProgression = 100 downTo 2 step 2
+    numerosParesReverso.forEach { print("$it ") }
 
-    numerosParesReverso.forEach { print("$it ")}
+    val salarios: DoubleArray = doubleArrayOf(1500.55, 9100.0, 2990.99, 8500.0, 10000.0)
 
-    println()
-
-    val intervalo = 1500.0..5000.0
-    val salario = 5000.1
-    if(salario in intervalo){
-        println("Esta dentro do padrao")
-    }else{
-        println("Não se encontra no intervalo")
+    val base = 1500.0
+    val topo = 4000.0
+    var contador = 0
+    for (salario in salarios) {
+        if(salario in base..topo) {
+            contador++
+        }
     }
 
     val alfabeto: CharRange = 'a'..'z'
-    if('k' in alfabeto){
-        println("Esta no alfabeto")
+
+    alfabeto.forEach { print("$it ") }
+
+    if('k' in alfabeto) {
+        println("O caractere 'k' está contido em $alfabeto")
     }
 }
